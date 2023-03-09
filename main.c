dc75d897ed7e7ad775208c659df20e7deb9c1163
230500226 <125464717+230500226@users.noreply.github.com>
2023-03-09 13:10:52 +0000

    double scan to pass char scan bug

--- main.c
+++ main.c
@@ -20,12 +20,12 @@ int main(void)
     printf("Please enter a number to check if it is positive, negative or zero\n");
     scanf("%d", &n3);
     if (n3>0){
-        printf("%d is positive\n", n3);
+        printf("%d is positive\n\n", n3);
     } else if (n3<0){
-        printf("%d is negative\n", n3);
+        printf("%d is negative\n\n", n3);
         n3=n3*-1;
     } else {
-        printf("%d is zero\n", n3);
+        printf("%d is zero\n\n", n3);
     }
 
     //Question 3
@@ -40,23 +40,25 @@ int main(void)
 
     printf("Please enter a character to check if it is a vowel\n");
     scanf("%c", &c1);
+    scanf("%c", &c1);
     
     if (c1=='a' || c1=='e' || c1=='i' || c1=='o' || c1=='u'
     ||  c1=='A' || c1=='E' || c1=='I' || c1=='O' || c1=='U'){
-        printf("The character in Question 4 is a vowel\n");
+        printf("The character is a vowel\n");
     } else {
-        printf("The character in Question 4 is a consonant\n\n");
+        printf("The character is a consonant\n\n");
     }
 
     //Question 5
     char c2;
     
-    printf("Please enter a character to check if it is an alphabet letter, a digit or a special character\n");
+    printf("Please enter a character to check if it is an alphabet letter, a digit or a special character\n\n");
+    scanf("%c", &c2);
     scanf("%c", &c2);
     if (c2=='1' || c2=='2'|| c2=='3'|| c2=='4'|| c2=='5'|| c2=='6'|| c2=='7'|| c2=='8'|| c2=='9'|| c2=='0'){
-        printf("You have entered a digit");
+        printf("You have entered a digit\n\n");
     } else if (c2=='a' || c2=='b' || c2=='c' || c2=='d' || c2=='e' || c2=='f' || c2=='g' || c2=='h' || c2=='i' || c2=='j' || c2=='k' || c2=='l' || c2=='m' || c2=='n' ||c2=='o' || c2=='p' || c2=='q' || c2=='r' || c2=='s' || c2=='t' || c2=='u' || c2=='v' || c2=='w' || c2=='x' || c2=='y' || c2=='z'){
-        printf("You have entered an alphabet letter\n");
+        printf("You have entered an alphabet letter\n\n");
     } else {
         printf("You have entered a special character or a capital \n\n");
     }
@@ -65,7 +67,7 @@ int main(void)
     int SP, CP;
 
     printf("Please enter the cost price and the selling price of the product\n");
-    scanf("%d%d", &CP, &P);
+    scanf("%d%d", &CP, &SP);
     if (SP>CP){
         int profit = SP - CP;
         printf("The total profit is %d\n\n", profit);
